3cc2319ae3c7af6d39ddff31f9b49d22620c58ba
Muhammad Inam <nudmaufon@gmail.com>
2024-11-09 23:49:45 +0500

    create-helper-function

--- src/slices/HeroPage/index.tsx
+++ src/slices/HeroPage/index.tsx
@@ -1,10 +1,9 @@
-"use client"
+"use client";
 import { Content, KeyTextField } from "@prismicio/client";
 import { SliceComponentProps } from "@prismicio/react";
 import gsap, { random } from "gsap";
-import {useEffect , useRef} from "react";
-
-
+import { useEffect, useRef } from "react";
+import Bounded from "@/components/Bounded";
 /**
  * Props for `HeroPage`.
  */
@@ -17,44 +16,60 @@ const HeroPage = ({ slice }: HeroPageProps): JSX.Element => {
   const component = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
-    let ctx = gsap.context(( ) => {
+    let ctx = gsap.context(() => {
       const tl = gsap.timeline();
-      tl.fromTo(".name-animation", {
-        x:-100,
-        opacity:0,
-        rotate: -10,
-      },
-      {
-        x:0,
-        opacity:1,
-        rotate: 0,
-        duration: 1,
-        ease: "elastic.out(1, 0.3)",
-        transformOrigin: "left top",
-        stagger: {
-          each:0.1,
-          from: "random"
+      tl.fromTo(
+        ".name-animation",
+        {
+          x: -100,
+          opacity: 0,
+          rotate: -10,
+        },
+        {
+          x: 0,
+          opacity: 1,
+          rotate: 0,
+          duration: 1,
+          ease: "elastic.out(2, 0.6)",
+          transformOrigin: "right top",
+          stagger: {
+            each: 0.1,
+            from: "random",
+          },
         }
-      }
-    )
-    }, component)
-    return () => ctx.revert()
-  }, [])
+      );
+      tl.fromTo(
+        ".tag-name",
+        {
+          y: 20,
+          opacity: 0,
+          duration: 0.7,
+        },
+        {
+          y: 0,
+          opacity: 1,
+          duration: 0.7,
+          ease: "elastic.out(2, 0.2)",
+        }
+      );
+    }, component);
+    return () => ctx.revert();
+  }, []);
 
-
-
-  const renderLetter = ( name:KeyTextField , key:string ) => {
-    if (!name) return ;
-    return (
-      name.split("").map((letter, index) => (
-        <span key={index} className={`name-animation name-animation-${key} opacity- inline-block`}>{letter}</span>
-      ))
-    )
-  }
-
+  const renderLetter = (name: KeyTextField, key: string) => {
+    if (!name) return;
+    return name.split("").map((letter, index) => (
+      <span
+        key={index}
+        className={`name-animation name-animation-${key} opacity- inline-block`}
+      >
+        {letter}
+      </span>
+    ));
+  };
 
   return (
-    <section
+    <Bounded
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
       ref={component}
@@ -65,13 +80,19 @@ const HeroPage = ({ slice }: HeroPageProps): JSX.Element => {
             className="mb-8 text-[clamp(3rem,15vmin,15rem)] font-extrabold leading-none tracking-tighter "
             aria-label={slice.primary.name + " " + slice.primary.lastname}
           >
-            <span className="block text-slate-300">{renderLetter(slice.primary.name, "first")}</span>
-            <span className="block text-slate-500 -mt-[.2em]">{renderLetter(slice.primary.lastname, "lastName")}</span>
+            <span className="block text-slate-300">
+              {renderLetter(slice.primary.name, "first")}
+            </span>
+            <span className="block text-slate-500 -mt-[.2em]">
+              {renderLetter(slice.primary.lastname, "lastName")}
+            </span>
           </h1>
-            <span className="block bg-gradient-to-tr from-yellow-500 via-yellow-200 to-yellow-500 bg-clip-text text-transparent text-2xl font-bold uppercase tracking-[.2em] opacity-100 md:text-4xl">{slice.primary.tagline}</span>
+          <span className="block bg-gradient-to-tr from-yellow-500 via-yellow-200 to-yellow-500 bg-clip-text text-transparent text-2xl font-bold uppercase tracking-[.2em] opacity-100 md:text-4xl tag-name">
+            {slice.primary.tagline}
+          </span>
         </div>
       </div>
-    </section>
+    </Bounded>
   );
 };
 
