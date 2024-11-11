f6827c4827133fb31f20a50f6abdcaca91845aea
Muhammad Inam <nudmaufon@gmail.com>
2024-11-11 23:35:04 +0500

    few-more-bugs🐞

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   turboMode: false,
+  transpilePackages: ['three'],
+
 };
 
 export default nextConfig;
