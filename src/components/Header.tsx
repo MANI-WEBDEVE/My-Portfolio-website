f7fe58de4bcfc970196cec991db664390cbac574
Muhammad Inam <nudmaufon@gmail.com>
2024-11-14 12:50:15 +0500

    navbar-ui-desigm-start

---
+++ src/components/Header.tsx
@@ -0,0 +1,38 @@
+import Link from "next/link";
+import React from "react";
+
+const Header = () => {
+  return (
+    <>
+      <header className="top-0 z-50 mx-auto max-w-7xl md:sticky md:top-4">
+        <nav className="flex items-center justify-between">
+          <div>
+            <ul>
+              <li>
+                <Link href={"/"} aria-label="home-page">
+                  M-Inam
+                </Link>
+              </li>
+            </ul>
+          </div>
+          <div>
+          <ul className="flex items-center gap-4">
+              <li>
+                <Link href={"/about"} aria-label="home-page">
+                  About
+                </Link>
+              </li>
+              <li>
+                <Link href={"/"} aria-label="home-page">
+                  About
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </header>
+    </>
+  );
+};
+
+export default Header;
