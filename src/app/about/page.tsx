3bc1850600a0487c2cda7c7c3c77604fcf21d762
Muhammad Inam <nudmaufon@gmail.com>
2024-11-14 21:58:34 +0500

    create-about-page-content-and-animation-image

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,11 +1,55 @@
-import React from 'react'
+import Avatar from "@/components/Avatar";
+import Bounded from "@/components/Bounded";
+import Button from "@/components/Button";
+import Heading from "@/components/Heading";
+import { MoveUpRight } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
 
 const page = () => {
   return (
-    <div>
-      About
-    </div>
-  )
-}
+    <>
+      <Bounded>
+        <div className="grid gap-x-8 gap-y-6 md:grid-cols-[2fr,1fr]">
+          <Heading as="h1" size="xl" className="col-start-1">
+            About Inam
+          </Heading>
+          <div className="prose prose-xl prose-slate prose-invert col-start-1 mt-4">
+            <p>
+              As a dedicated full-stack developer with a strong foundation in
+              both front-end and back-end technologies, I specialize in building
+              high-quality, scalable web applications. I’m proficient in tools
+              and frameworks like Next.js, React.js, Node.js, and Express
+            </p>
 
-export default page
+            <p>
+              My passion for coding and problem-solving drives me to create
+              seamless user experiences and efficient server-side functionality.
+             
+            </p>
+
+            <p>
+              I'm eager to tackle challenging problems and am always on the
+              lookout for new tools and best practices to enhance my development
+              process.
+            </p>
+          </div>
+          <Link
+            href="#"
+            className="w-fit relative inline-block px-5 py-2 font-semibold text-gray-900 border border-gray-900 rounded-md bg-white overflow-hidden group hover:text-black transform transition-transform duration-300 ease-out mt-4 hover:scale-105"
+          >
+            <span className="absolute bottom-0 inset-0 w-full h-1 bg-yellow-500 transition-all duration-300 ease-out group-hover:h-full group-hover:bottom-0"></span>
+            <span className="relative items-center flex gap-2 justify-center">
+              Resume
+              <MoveUpRight className="w-4 h-4" />
+            </span>
+          </Link>
+            <Avatar src={"/image/ceo3.jpg"} alt={"ceo"} width={245} height={245} />
+        </div>
+      </Bounded>
+    </>
+  );
+};
+
+export default page;
